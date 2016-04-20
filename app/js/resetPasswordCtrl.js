fbcb63d2a83a004e2a1d6ad96268e02b54d72bfe
Max Häkkä <hakka@kth.se>
2016-04-20 18:41:06 +0200

    Added email service for password reset.

--- app/js/resetPasswordCtrl.js
+++ app/js/resetPasswordCtrl.js
@@ -1,3 +1,11 @@
 onlineClothingStoreApp.controller('ResetPasswordCtrl', function ($scope, Service) {
 //send an email to the customer with a new randomized password
+$scope.not_sent=true;
+$scope.sent=false;
+  $scope.resetPassword=function(){
+      Service.resetPassword($scope.email);
+      $scope.sent=true;
+      $scope.not_sent=false;
+  }
+
  });
