4644a4b6241701c9347b7580599136be8f6db90a
AZNanoi <ezinanoi@gmail.com>
2016-04-21 03:10:18 +0200

    Added dropdown menu icon in the header and its function.

--- app/js/navbarCtrl.js
+++ app/js/navbarCtrl.js
@@ -1,4 +1,6 @@
 onlineClothingStoreApp.controller('NavbarCtrl', function ($scope, Service) {
+	$scope.menuIcon = "images/menu-icon.png";
+	$scope.dMenuDisplayed = false;
 	$scope.itemsAmount=Service.getCart().length;
 
 	var parentScope = $scope.$parent.$parent;
@@ -24,5 +26,16 @@ onlineClothingStoreApp.controller('NavbarCtrl', function ($scope, Service) {
 	$scope.logOut = function(){
 		Service.logOut();
 		authDataCallback();
+		$scope.dMenuDisplayed = false;
+	}
+
+	$scope.toggleDMenu = function(event) {
+		if($scope.dMenuDisplayed == false){
+			$scope.dMenuDisplayed = true;
+			$(event.target).css("background-image", "url(images/menu-icon-gray.png)");
+		}else{
+			$scope.dMenuDisplayed = false;
+			$(event.target).css("background-image", "url(images/menu-icon.png)");
+		}
 	}
 });
