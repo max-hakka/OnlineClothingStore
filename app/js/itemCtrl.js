158cd37c0fa2e5fb23496456b62e34013cdc399a
AZNanoi <ezinanoi@gmail.com>
2016-04-22 23:07:35 +0200

    Updated layout for "item" page and function for selecting size and color

--- app/js/itemCtrl.js
+++ app/js/itemCtrl.js
@@ -1,6 +1,7 @@
 onlineClothingStoreApp.controller('ItemCtrl', function ($scope, $routeParams, Service, $cookieStore) {
 	$scope.childScope = {};
 	$scope.amount = 0;
+	$scope.sizes = ["S", "M", "L"];
 	var quantities;
 	var itemId = $routeParams.itemId;
 	Service.getItem.get({"id":itemId}, function(data){
@@ -23,24 +24,22 @@ onlineClothingStoreApp.controller('ItemCtrl', function ($scope, $routeParams, Se
 		var list = [];
 		if($scope.selectedSize){
 			for (i=0; i < quantities.length; i++){
-				if ($scope.selectedSize.Size==quantities[i].Size){
+				if ($scope.selectedSize==quantities[i].Size && $.inArray(quantities[i].Color, list)==-1){
 					list.push(quantities[i].Color);
-				}			
+				}
 			}
 			$scope.colors=list;
 		}else{
 			$scope.colors=undefined;
 		}
-
 	}
 
 	$scope.getAmount = function(){
 		console.log($scope.selectedColor);
-
 		var n = 0;
 		for (i=0; i < quantities.length; i++){
-			if ($scope.selectedSize.Size==quantities[i].Size && $scope.selectedColor==quantities[i].Color){
-				n+=1;
+			if ($scope.selectedSize==quantities[i].Size && $scope.selectedColor==quantities[i].Color){
+				n=quantities[i].Quantity;
 			}
 		}
 		$scope.amount=n;
